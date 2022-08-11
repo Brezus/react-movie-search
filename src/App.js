aabc97459b714fb0e7f104b4a2b20f6f21ff6f78
Roshane Miller <millerroshane44@gmail.com>
2022-08-11 01:27:27 -0400

    hooks folder
    created hooks folder and other components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// process.env.REACT_APP_API_KEY
+import Nav from "./components/Nav"
+import styled from "styled-components"
+import Homescreen from "./components/Homescreen"
+import Main from "./components/Main"
 
+const DivApp = styled.div`
+  position: relative;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DivApp>
+      <Nav />
+      <Homescreen />
+      <Main />
+    </DivApp>
+  )
 }
 
-export default App;
+export default App
