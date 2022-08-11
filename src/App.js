04bca6da6d8704baeca285948250fa00ac77183c
Roshane Miller <millerroshane44@gmail.com>
2022-08-11 04:20:26 -0400

    homescreen component background

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import Nav from "./components/Nav"
 import styled from "styled-components"
 import Homescreen from "./components/Homescreen"
 import Main from "./components/Main"
+import { ThemeProvider } from "styled-components"
+import { useEffect, useState } from "react"
+import { colors } from "./variables/Vars"
+
+const key = process.env.REACT_APP_API_KEY
+const baseBackdropImgUrl = "http://image.tmdb.org/t/p/original"
+const basePosterImgUrl = "http://image.tmdb.org/t/p/w780"
+const sliceVal = 20
+const moviesAndTvUrl =
+  "https://api.themoviedb.org/3/trending/all/day?api_key=key"
+const tvUrl = "https://api.themoviedb.org/3/trending/tv/day?api_key=key"
+const movieUrl = "https://api.themoviedb.org/3/trending/movie/day?api_key=key"
 
 const DivApp = styled.div`
   position: relative;
 `
 function App() {
   return (
-    <DivApp>
-      <Nav />
-      <Homescreen />
-      <Main />
-    </DivApp>
+    <ThemeProvider theme={colors}>
+      <DivApp>
+        <Nav />
+        <Homescreen />
+        <Main />
+      </DivApp>
+    </ThemeProvider>
   )
 }
 
