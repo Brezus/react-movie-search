71264dd8940f091dcaa55b10c845c4596812c2e2
Roshane Miller <millerroshane44@gmail.com>
2022-08-15 01:12:04 -0400

    implemented debounce
    used loadash library debounce function to limit api request made

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,24 +1,60 @@
 import React, { useState, useEffect } from "react"
 import styled from "styled-components"
+import { nanoid } from "nanoid"
 
 const Div = styled.div`
   min-height: 100vh;
+  padding: 5em 0;
   padding-top: 10rem;
+  display: flex;
+  flex-direction: column;
   width: 95%;
   margin-inline: auto;
+  gap: 3em;
 `
-
 const Span = styled.span`
   display: block;
   font-size: 3rem;
 `
+const Section = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 2em;
+  border-radius: 10px;
+  justify-content: center;
+  overflow: hidden;
+`
+const Movie = styled.div`
+  display: grid;
+  grid-template-rows: 300px auto;
+  grid-template-columns: minmax(150px, 1fr);
+`
+const Poster = styled.div`
+  background-size: cover;
+  background-position: center;
+`
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  padding: 1em 0;
+`
+const MetaInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const TypeIndicator = styled.p`
+  padding: 0.2em 0.8em;
+  background: transparent;
+  border: 1px solid whitesmoke;
+  border-radius: 5px;
+`
 
 export default function SearchPage({ movieName }) {
   const [data, setData] = useState([])
-  const [newSearch, setNewSearch] = useState()
   useEffect(() => {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${movieName}&page=1&include_adult=false`
-
     fetch(url)
       .then((res) => {
         if (!res.ok) {
@@ -28,16 +64,42 @@ export default function SearchPage({ movieName }) {
         }
       })
       .then((data) => {
-        setData(data)
-        console.log(data)
+        setData(data.results)
       })
       .catch((err) => {
         console.error(err)
       })
   }, [movieName])
+  const movies = data?.map((movie) => {
+    return (
+      <Movie key={nanoid()}>
+        <Poster
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/w780${movie.poster_path})`,
+          }}
+        />
+        <Info>
+          <h3>
+            {movie.title.length >= 25
+              ? movie.title?.slice(0, 25) + "..."
+              : movie.title}
+          </h3>
+          <MetaInfo>
+            <p>
+              {movie.release_date?.slice(0, 4)} • ({movie.vote_average} ⭐)
+            </p>
+            <TypeIndicator>movie</TypeIndicator>
+          </MetaInfo>
+        </Info>
+      </Movie>
+    )
+  })
   return (
     <Div style={{ minHeight: "100vh" }}>
-      Search Results for <Span>{movieName}</Span>
+      <p>
+        Search Results for <Span>{movieName}</Span>
+      </p>
+      <Section>{movies}</Section>
     </Div>
   )
 }
