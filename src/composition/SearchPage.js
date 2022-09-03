2628927c4d7e6415382ce0a8e14fd26a474b8d76
Roshane Miller <millerroshane44@gmail.com>
2022-09-03 16:11:01 -0400

    fixed size of horizontal scroll els

--- src/composition/SearchPage.js
+++ src/composition/SearchPage.js
@@ -148,7 +148,7 @@ function SearchPage({
               backgroundBlendMode: `${
                 horizontalScroll ? "multiply" : "normal"
               }`,
-              maxWidth: "300px",
+              maxWidth: `${!horizontalScroll && "300px"}`,
               gridRow: "1",
               gridColumn: "1",
             }}
