e1b047906906499cfeccb23bb873c5312c35c645
Roshane Miller <millerroshane44@gmail.com>
2022-09-22 14:51:12 -0400

    renamed files

---
+++ src/css-context/Vars.js
@@ -0,0 +1,19 @@
+export const vars = {
+  gBackground: "rgb(252, 252, 252, 0)",
+  darkBg: "rgba(16, 21, 30, 1)",
+  gBackgroundGradient: `linear-gradient(
+        180deg,
+        rgba(252, 252, 252, 0) 30%,
+        rgba(16, 21, 30, 1) 100%
+      );`,
+  hScreenHeight: "400px",
+  gradientHight: "120px",
+  iconColor: "white",
+  inputHeight: "48px",
+  white: "white",
+  border: "20px",
+  darkBlue: "#2470FF",
+  darkYellow: "#F5AB0A",
+  darkerYellow: "#A87300",
+  fontSize: "3rem",
+}
